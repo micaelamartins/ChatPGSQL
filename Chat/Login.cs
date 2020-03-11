d8c3e8064a2b520aff7d643221cc5d11211c6493
hospitalEstg <55716589+hospitalEstg@users.noreply.github.com>
2020-03-11 17:12:34 +0000

    codigo comentado v2

--- Chat/Login.cs
+++ Chat/Login.cs
@@ -22,10 +22,11 @@ namespace Chat
         {
             InitializeComponent();
         }
-
+        //
         private void Login_Load(object sender, EventArgs e)
         {
             getConnectionString();
+            //set dots on password
             textbox_password.UseSystemPasswordChar = true;
         }
 
@@ -33,7 +34,7 @@ namespace Chat
         {
             string username = textbox_username.Text;
             string password = textbox_password.Text;
-
+            //convert password to sha256
             byte[] hash;
             using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
             {
@@ -42,36 +43,44 @@ namespace Chat
             }
             string result = System.Text.Encoding.UTF8.GetString(hash);
 
-            DataTable dt = new DataTable();
+
+            
+            
             bool connectionWorks = getConnectionString();
             if (connectionWorks == true)
             {
+                //open connection
                 NpgsqlConnection con = new NpgsqlConnection(conn);
                 con.Open();
+                //get all users and execute the query and convert it to a list
+                DataTable dt = new DataTable();
                 String all_users = "SELECT *  FROM users";
                 NpgsqlCommand cmd = new NpgsqlCommand(all_users, con);
                 dt.Load(cmd.ExecuteReader());
                 List<DataRow> drList = dt.AsEnumerable().ToList();
 
                 try
-                {
-                    bool cont = true;
+                { 
 
+                    bool cont = true;
+                    //compare all values from list to the username inserted
                     foreach (DataRow str in drList)
                     {
                         if (str.ItemArray[0].ToString().Equals(textbox_username.Text))
                         {
+                            //if username already exits dont let create a new user
                             cont = false;
                             MessageBox.Show("Ja existe o Username");
                         }
                     }
-
+                    //if not exists the username
                     if (cont == true)
                     {
+                        // insert user on db
                         string sql1 = "INSERT INTO users(username, password) VALUES ('" + username.ToString() + "','" + result.ToString() + "')";
                         NpgsqlCommand cmdo = new NpgsqlCommand(sql1, con);
-
-                        //dbcmd.CommandText = sql1;
+                        //executes the query
+                        
                         cmdo.ExecuteNonQuery();
                         lb_alert.Text = "Registo Efetuado com Sucesso!";
                         con.Close();
@@ -88,6 +97,7 @@ namespace Chat
             string username = textbox_username.Text;
             string password = textbox_password.Text;
 
+            // convert the password inserted to sha256 and then compares it to the hash saved on db
 
             byte[] hash;
             using (SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider())
@@ -96,25 +106,33 @@ namespace Chat
                 hash = hashdata;
             }
 
+
             DataTable dt = new DataTable();
             bool connectionWorks = getConnectionString();
             if (connectionWorks == true)
             {
+                //saves the hash on a string
                 string result = System.Text.Encoding.UTF8.GetString(hash);
+                //starts connection
+
                 NpgsqlConnection con = new NpgsqlConnection(conn);
-
-
                 con.Open();
+                // get all users from db
                 String all_users = "SELECT *  FROM users";
-                bool cont = false;
 
+                bool cont = false;
+                //execute query and save it on a list
                 NpgsqlCommand cmd = new NpgsqlCommand(all_users, con);
                 dt.Load(cmd.ExecuteReader());
                 List<DataRow> drList = dt.AsEnumerable().ToList();
+
                 foreach (DataRow str in drList)
                 {
+                    //compares if the username and password inserted on texboxs is on db
                     if (str.ItemArray[0].ToString().Equals(textbox_username.Text) && str.ItemArray[1].Equals(result))
                     {
+
+                        //if the users login in sucefull goes to another form (chat) and send the username
                         cont = true;
                         Chat ga = new Chat(textbox_username.Text);
                         this.Hide();
@@ -123,10 +141,12 @@ namespace Chat
                         break;
                     }
                 }
+                //if login fails
                 if (cont == false)
                 {
                     MessageBox.Show("Dados incorretos ou não está Registado!");
                 }
+                //close connection
                 con.Close();
             }
         }
@@ -136,6 +156,7 @@ namespace Chat
             //Tests the current connection
             try
             {
+
                 NpgsqlConnection testCurrentConnString = new NpgsqlConnection(conn);
                 testCurrentConnString.Open();
                 testCurrentConnString.Close();
